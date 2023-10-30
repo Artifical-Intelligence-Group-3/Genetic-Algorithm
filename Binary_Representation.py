526ae4cb0d241c3e488d4ede3ce3608d26d68a39
BukanRaychan <fasyaraihan82@gmail.com>
2023-10-30 12:48:17 +0700

    Update

--- Binary_Representation.py
+++ Binary_Representation.py
@@ -4,7 +4,7 @@ import os
 
 
 
-def main()
+def main():
     global LOWER_LIMIT, UPPER_LIMIT, POPULATION_SIZE, CHROMOSOME_LENGTH
     LOWER_LIMIT = -10
     UPPER_LIMIT = 10
@@ -17,40 +17,40 @@ def main()
     best_fitness = float('-inf')
     best_individual = ""
 
-    population = initializePopulation()
+    # population = initializePopulation()
 
-    while change_credit <= limitation_of_change:
-        # Decode Cromosom to individual
-        decoded_population = decodeChromosomeToIndividual(population)
+    # while change_credit <= limitation_of_change:
+    #     # Decode Cromosom to individual
+    #     decoded_population = decodeChromosomeToIndividual(population)
 
-        # Fitness Calculation
-        fitness_of_population = fitnessCalculation(decoded_population)
+    #     # Fitness Calculation
+    #     fitness_of_population = fitnessCalculation(decoded_population)
 
-        # Parent Selection
-        parent1, parent2 = parentSelection(fitness_of_population, population)
+    #     # Parent Selection
+    #     parent1, parent2 = parentSelection(fitness_of_population, population)
 
-        # Recombination
-        child1, child2 = recombination(parent1,parent2)
+    #     # Recombination
+    #     child1, child2 = recombination(parent1,parent2)
         
-        # permutation
-        mutated_child1, mutated_child2 = mutation(child1, child2)
+    #     # permutation
+    #     mutated_child1, mutated_child2 = mutation(child1, child2)
 
-        # Exchange Population
-        population = exchangePopulation(population, fitness_of_population, mutated_child1, mutated_child2)
+    #     # Exchange Population
+    #     population = exchangePopulation(population, fitness_of_population, mutated_child1, mutated_child2)
 
-        # if best_fitness < max(fitnessCalculation(population)):
-        #     best_fitness = max(fitnessCalculation(population))
-        #     best_individual = population[fitness_of_population.index(max(fitness_of_population))]
-        #     change_credit = 0
+    #     # if best_fitness < max(fitnessCalculation(population)):
+    #     #     best_fitness = max(fitnessCalculation(population))
+    #     #     best_individual = population[fitness_of_population.index(max(fitness_of_population))]
+    #     #     change_credit = 0
 
-        # os.system('cls')
-        # print(f'Generation : {generation}\n')
-        # print(f'BEST INDIVIDUAL')
-        # print(f'x1 : {best_individual[0]}')
-        # print(f'x2 : {best_individual[1]}\n')
-        # print(f'BEST FITNESS: {best_fitness}\n')
+    #     # os.system('cls')
+    #     # print(f'Generation : {generation}\n')
+    #     # print(f'BEST INDIVIDUAL')
+    #     # print(f'x1 : {best_individual[0]}')
+    #     # print(f'x2 : {best_individual[1]}\n')
+    #     # print(f'BEST FITNESS: {best_fitness}\n')
 
-        # change_credit += 1
-        # generation += 1
+    #     # change_credit += 1
+    #     # generation += 1
 
 main()
